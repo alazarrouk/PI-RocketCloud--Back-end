a0abff738a21902b0d95ebf51b354c33ca0216fb
FuturDev22 <souissi.oumaima@esprit.tn>
2023-05-03 17:33:51 +0200

    commit

--- src/main/java/rocketcloud/pidevbackend/controllers/PanierController.java
+++ src/main/java/rocketcloud/pidevbackend/controllers/PanierController.java
@@ -1,34 +1,25 @@
 package rocketcloud.pidevbackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rocketcloud.pidevbackend.entities.Paiement;
 import rocketcloud.pidevbackend.entities.Panier;
 import rocketcloud.pidevbackend.services.CommandeService;
 import rocketcloud.pidevbackend.services.PanierService;
+import rocketcloud.pidevbackend.entities.Produit;
+import rocketcloud.pidevbackend.entities.User;
+import rocketcloud.pidevbackend.repositories.ProduitRepository;
 
+
+
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/panier")
 public class PanierController {
-    @Autowired
-    private PanierService panierService;
-    @PostMapping("/add")
-    public Panier create_panier(@RequestBody Panier panier) {
-        return panierService.create_panier(panier);
-    }
-    @GetMapping("/get")
-    public List<Panier> get_paniers(){
-        return panierService.get_paniers();
-    }
-    @GetMapping("/get/{id}")
-    public Panier get_panier_by_id(@PathVariable("id") Integer id) {
-        return panierService.get_panier(id);
-    }
-    @DeleteMapping("/delete/{id}")
-    public void delete_panier(@PathVariable("id") Integer id ) {
-        panierService.delete_panier(id);
-    }
+
 
 }
